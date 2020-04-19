b2282355031ce8789405268ded5322c93dfc6211
kirill5k <immotional@aol.com>
2020-04-19 20:04:18 +0100

    initial commit

---
+++ build.sbt
@@ -0,0 +1,18 @@
+import Dependencies.Libraries._
+
+ThisBuild / scalaVersion     := "2.13.1"
+ThisBuild / version          := "0.0.1-SNAPSHOT"
+ThisBuild / organization     := "io.kirill"
+ThisBuild / organizationName := "kirill"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "mongo4cats",
+    libraryDependencies ++= Seq(
+      scalaMongoDb,
+      catsCore,
+      catsEffect,
+      catsEffectTest % Test,
+      scalaTest % Test
+    )
+  )
---
+++ project/Dependencies.scala
@@ -0,0 +1,21 @@
+import sbt._
+
+object Dependencies {
+  object Versions {
+    lazy val scalaMongoDb   = "2.9.0"
+    lazy val catsCore       = "2.1.3"
+    lazy val catsEffect     = "2.1.1"
+    lazy val catsEffectTest = "0.4.0"
+    lazy val scalaTest      = "3.1.1"
+  }
+
+  object Libraries {
+    lazy val scalaMongoDb = "org.mongodb.scala" %% "mongo-scala-driver" % Versions.scalaMongoDb
+
+    lazy val catsCore   = "org.typelevel" %% "cats-core"   % Versions.catsCore
+    lazy val catsEffect = "org.typelevel" %% "cats-effect" % Versions.catsEffect
+
+    lazy val scalaTest      = "org.scalatest"  %% "scalatest"                     % Versions.scalaTest
+    lazy val catsEffectTest = "com.codecommit" %% "cats-effect-testing-scalatest" % Versions.catsEffectTest
+  }
+}
