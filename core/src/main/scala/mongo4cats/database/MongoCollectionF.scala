ac1a1d03d8bf32a1ad99e508f2d10d5dc5483ad2
kirill5k <immotional@aol.com>
2020-10-09 20:46:50 +0100

    collection methods

--- core/src/main/scala/mongo4cats/database/MongoCollectionF.scala
+++ core/src/main/scala/mongo4cats/database/MongoCollectionF.scala
@@ -1,12 +1,12 @@
 package mongo4cats.database
 
-import cats.effect.{Async, Concurrent, Sync}
-import fs2.concurrent.Queue
+import cats.effect.Async
 import mongo4cats.database.helpers._
 import mongo4cats.database.queries.QueryBuilder
 import org.bson.conversions.Bson
-import org.mongodb.scala.result.InsertOneResult
-import org.mongodb.scala.{Document, MongoCollection, MongoNamespace}
+import org.mongodb.scala.model._
+import org.mongodb.scala.result._
+import org.mongodb.scala.{MongoCollection, MongoNamespace, SingleObservable}
 
 import scala.reflect.ClassTag
 
@@ -19,23 +19,92 @@ final class MongoCollectionF[T: ClassTag] private(
 
   //watch
   //aggregate
-  //update
-  //insertMany
   //bulk
-  //delete
+  //findOneAndReplace
+  //findOneAndDelete
+  //findOneAndUpdate
   //case classes
+  //createIndexes
+  //distinct
+
+
+  def dropIndex[F[_]: Async](name: String): F[Unit] =
+    doAsyncVoid(collection.dropIndex(name))
+
+  def dropIndex[F[_]: Async](keys: Bson): F[Unit] =
+    doAsyncVoid(collection.dropIndex(keys))
+
+  def dropIndex[F[_]: Async](keys: Bson, options: DropIndexOptions): F[Unit] =
+    doAsyncVoid(collection.dropIndex(keys, options))
+
+  def dropIndexes[F[_]: Async](options: DropIndexOptions): F[Unit] =
+    doAsyncVoid(collection.dropIndexes(options))
+
+  def dropIndexes[F[_]: Async](): F[Unit] =
+    doAsyncVoid(collection.dropIndexes())
 
   def drop[F[_]: Async](): F[Unit] =
-    Async[F].async { k =>
-      collection.drop().subscribe(voidObserver(k))
-    }
+    doAsyncVoid(collection.drop())
+
+  def createIndex[F[_]: Async](filters: Bson): F[String] =
+    doAsync(collection.createIndex(filters))
+
+  def createIndex[F[_]: Async](filter: Bson, options: IndexOptions): F[String] =
+    doAsync(collection.createIndex(filter, options))
+
+  def updateMany[F[_]: Async](filters: Bson, update: Bson): F[UpdateResult] =
+    doAsync(collection.updateMany(filters, update))
+
+  def updateMany[F[_]: Async](filters: Bson, update: Seq[Bson]): F[UpdateResult] =
+    doAsync(collection.updateMany(filters, update))
+
+  def updateMany[F[_]: Async](filter: Bson, update: Bson, options: UpdateOptions): F[UpdateResult] =
+    doAsync(collection.updateMany(filter, update, options))
+
+  def updateMany[F[_]: Async](filter: Bson, update: Seq[Bson], options: UpdateOptions): F[UpdateResult] =
+    doAsync(collection.updateMany(filter, update, options))
+
+  def updateOne[F[_]: Async](filters: Bson, update: Bson): F[UpdateResult] =
+    doAsync(collection.updateOne(filters, update))
+
+  def updateOne[F[_]: Async](filters: Bson, update: Seq[Bson]): F[UpdateResult] =
+    doAsync(collection.updateOne(filters, update))
+
+  def updateOne[F[_]: Async](filter: Bson, update: Bson, options: UpdateOptions): F[UpdateResult] =
+    doAsync(collection.updateOne(filter, update, options))
+
+  def updateOne[F[_]: Async](filter: Bson, update: Seq[Bson], options: UpdateOptions): F[UpdateResult] =
+    doAsync(collection.updateOne(filter, update, options))
+
+  def replaceOne[F[_]: Async](filters: Bson, replacement: T): F[UpdateResult] =
+    doAsync(collection.replaceOne(filters, replacement))
+
+  def replaceOne[F[_]: Async](filter: Bson, replacement: T, options: ReplaceOptions): F[UpdateResult] =
+    doAsync(collection.replaceOne(filter, replacement, options))
+
+  def deleteOne[F[_]: Async](filters: Bson): F[DeleteResult] =
+    doAsync(collection.deleteOne(filters))
+
+  def deleteOne[F[_]: Async](filter: Bson, options: DeleteOptions): F[DeleteResult] =
+    doAsync(collection.deleteOne(filter, options))
+
+  def deleteMany[F[_]: Async](filters: Bson): F[DeleteResult] =
+    doAsync(collection.deleteMany(filters))
+
+  def deleteMany[F[_]: Async](filter: Bson, options: DeleteOptions): F[DeleteResult] =
+    doAsync(collection.deleteMany(filter, options))
 
   def insertOne[F[_]: Async](document: T): F[InsertOneResult] =
-    Async[F].async { k =>
-      collection
-        .insertOne(document)
-        .subscribe(singleItemObserver[InsertOneResult](k))
-    }
+    doAsync(collection.insertOne(document))
+
+  def insertOne[F[_]: Async](document: T, options: InsertOneOptions): F[InsertOneResult] =
+    doAsync(collection.insertOne(document, options))
+
+  def insertMany[F[_]: Async](documents: Seq[T]): F[InsertManyResult] =
+    doAsync(collection.insertMany(documents))
+
+  def insertMany[F[_]: Async](documents: Seq[T], options: InsertManyOptions): F[InsertManyResult] =
+    doAsync(collection.insertMany(documents, options))
 
   def find(): QueryBuilder[T] =
     QueryBuilder(collection.find())
@@ -44,13 +113,22 @@ final class MongoCollectionF[T: ClassTag] private(
     QueryBuilder(collection.find(filter))
 
   def count[F[_]: Async](): F[Long] =
-    Async[F].async { k =>
-      collection.countDocuments().subscribe(singleItemObserver[Long](k))
-    }
+    doAsync(collection.countDocuments())
 
   def count[F[_]: Async](filter: Bson): F[Long] =
+    doAsync(collection.countDocuments(filter))
+
+  def count[F[_]: Async](filter: Bson, options: CountOptions): F[Long] =
+    doAsync(collection.countDocuments(filter, options))
+
+  private def doAsync[F[_]: Async, R](observable: => SingleObservable[R]): F[R] =
     Async[F].async { k =>
-      collection.countDocuments(filter).subscribe(singleItemObserver[Long](k))
+      observable.subscribe(singleItemObserver[R](k))
+    }
+
+  private def doAsyncVoid[F[_]: Async](observable: => SingleObservable[Void]): F[Unit] =
+    Async[F].async { k =>
+      observable.subscribe(voidObserver(k))
     }
 }
 
