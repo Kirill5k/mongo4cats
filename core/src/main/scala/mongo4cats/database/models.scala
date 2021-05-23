53459473c2d233c7f6fdc7af3e93f56715bbceb5
kirill5k <immotional@aol.com>
2021-05-23 09:54:52 +0100

    models type aliases

---
+++ core/src/main/scala/mongo4cats/database/models.scala
@@ -0,0 +1,10 @@
+package mongo4cats.database
+
+object models {
+  type Filters      = com.mongodb.client.model.Filters
+  type Updates      = com.mongodb.client.model.Updates
+  type Sorts        = com.mongodb.client.model.Sorts
+  type Accumulators = com.mongodb.client.model.Accumulators
+  type Aggregates   = com.mongodb.client.model.Aggregates
+  type Projections  = com.mongodb.client.model.Projections
+}
