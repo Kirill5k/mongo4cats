71fa56a563f2bf49fdb6c78958f49fd3de0dd9a7
kirill5k <immotional@aol.com>
2021-10-09 09:10:01 +0100

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object Dependencies {
   private object Versions {
-    val mongodb     = "4.3.1"
-    val fs2         = "3.1.0"
+    val mongodb     = "4.3.3"
+    val fs2         = "3.1.5"
     val scalaCompat = "2.5.0"
     val circe       = "0.14.1"
 
-    val logback   = "1.2.5"
-    val scalaTest = "3.2.9"
+    val logback   = "1.2.6"
+    val scalaTest = "3.2.10"
 
     val embeddedMongo   = "3.0.0"
     val immutableValue  = "2.8.8"
