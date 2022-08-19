af5739e9e664a9efc6511298761ac060168d1846
Ronan Michaux <ronan_michaux@yahoo.com>
2022-08-19 23:38:55 +0200

    Fix compile

--- bench/src/main/scala-2/mongo4cats/bench/DerivationWriteBench.scala
+++ bench/src/main/scala-2/mongo4cats/bench/DerivationWriteBench.scala
@@ -8,8 +8,6 @@ import org.openjdk.jmh.annotations._
 import org.openjdk.jmh.annotations.OutputTimeUnit
 import org.openjdk.jmh.annotations.Warmup
 import cats.syntax.all._
-import com.mongodb.internal.connection.ByteBufferBsonOutput
-import io.circe.{Decoder, Encoder, Json, ParsingFailure}
 import io.circe.syntax._
 import io.circe.generic.auto._
 import mongo4cats.circe._
@@ -18,19 +16,14 @@ import mongo4cats.derivation.bson.AllBsonDecoders._
 import mongo4cats.derivation.bson.derivation.decoder.auto._
 import mongo4cats.derivation.bson.derivation.encoder.auto._
 import mongo4cats.derivation.bson.{BsonDecoder, BsonEncoder, BsonValueOps}
-import mongo4cats.circe._
-import mongo4cats.codecs.MongoCodecProvider
 import org.bson.BsonBinaryWriter
 import org.bson.codecs.EncoderContext
-import org.bson.codecs.configuration.CodecRegistry
 import org.bson.io.BasicOutputBuffer
 
 import java.time.Instant
-import scala.util.chaining._
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeUnit.MILLISECONDS
 import java.util.concurrent.TimeUnit.SECONDS
-import scala.collection.immutable.HashSet
 
 final case class Person(
     firstName: String,
@@ -59,7 +52,7 @@ class DerivationWriteBench {
   val bsonEncoder    = BsonEncoder[Person]
   val cp             = deriveCirceCodecProvider[Person]
   val codec          = cp.get.get(classOf[Person], null)
-  val output         = new BasicOutputBuffer(100_000_000)
+  val output         = new BasicOutputBuffer(10 * 1000 * 1000)
 
   @Setup
   def setup(): Unit =
