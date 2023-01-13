cb29339fe94250f494d85878e3084f9e22a0401c
Domas Poliakas <domas@poliakas.dev>
2023-01-13 15:59:23 +0100

    Circe-converted decoders handle nulls, better errors

---
+++ circe/src/test/scala/mongo4cats/circe.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2020 Kirill5k
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mongo4cats
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+import io.circe.Json
+import org.bson.BsonString
+import io.circe.Decoder
+import org.scalatest.EitherValues
+
+class CirceSpec extends AnyWordSpec with Matchers with EitherValues {
+
+  "circe conversions" should {
+    "not explode when faced with null" in {
+      circe.implicits.circeDecoderToDecoder[Json].apply(null) should matchPattern { case Left(_) => }
+    }
+
+    "remove not report the internal root tag in history when reporting errors" in {
+
+      val deco = Decoder.instance(h => {
+        h.get[String]("hek")(Decoder.failedWithMessage("Bad!"))
+      })
+
+      val res = circe.implicits.circeDecoderToDecoder[String](deco).apply(new BsonString("hek"))
+
+      res.left.value.msg shouldBe "An error occured during decoding BsonValue BsonString{value='hek'}: DecodingFailure(Attempt to decode value on failed cursor, List(DownField(hek)))"
+
+    }
+  }
+
+}
