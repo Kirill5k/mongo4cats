a8740fc307cdf35071f37d01d448e3dda3248c5a
kirill5k <immotional@aol.com>
2023-03-08 09:26:17 +0000

    Bump dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,17 +4,17 @@ object Dependencies {
   private object Versions {
     val kindProjector      = "0.13.2"
     val cats               = "2.9.0"
-    val catsEffect         = "3.4.7"
+    val catsEffect         = "3.4.8"
     val mongodb            = "4.9.0"
     val fs2                = "3.6.1"
-    val circe              = "0.14.4"
+    val circe              = "0.14.5"
     val zio                = "2.0.9"
-    val zioInteropReactive = "2.0.0"
+    val zioInteropReactive = "2.0.1"
 
     val logback   = "1.4.5"
     val scalaTest = "3.2.15"
 
-    val embeddedMongo   = "4.5.1"
+    val embeddedMongo   = "4.6.0"
     val immutableValue  = "2.9.3"
     val commonsCompress = "1.22"
   }
