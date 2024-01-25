0985f632ccb9ed4cb2e16dabf286711eae3c30c3
kirill5k <immotional@aol.com>
2024-01-25 10:40:40 +0000

    Update tagline

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -23,8 +23,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title="Home"
+      description={siteConfig.tagline}>
       <HomepageHeader />
       <main>
         <section>
