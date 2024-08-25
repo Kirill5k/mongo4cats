9e36e80267ba0fbb6958d13cad1a555297078072
kirill5k <immotional@aol.com>
2024-08-25 09:28:14 +0100

    Bump dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,20 +2,20 @@ import sbt.*
 
 object Dependencies {
   private object Versions {
-    val mongodb            = "5.1.1"
+    val mongodb            = "5.1.3"
     val fs2                = "3.10.2"
     val kindProjector      = "0.13.3"
     val circe              = "0.14.9"
-    val zio                = "2.1.5"
+    val zio                = "2.1.8"
     val zioInteropReactive = "2.0.2"
-    val zioJson            = "0.7.1"
+    val zioJson            = "0.7.2"
 
-    val logback   = "1.5.6"
+    val logback   = "1.5.7"
     val scalaTest = "3.2.19"
 
-    val embeddedMongo   = "4.15.0"
+    val embeddedMongo   = "4.17.0"
     val immutableValue  = "2.10.1"
-    val commonsCompress = "1.26.2"
+    val commonsCompress = "1.27.1"
     val jsr305          = "3.0.2"
   }
 
--- version.sbt
+++ version.sbt
@@ -1,1 +1,1 @@
-ThisBuild / version := "0.7.9"
+ThisBuild / version := "0.7.9-SNAPSHOT"
