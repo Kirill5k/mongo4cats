6cc9881a718ff63de6b66cbff7b4807aec27aa9a
kirill5k <immotional@aol.com>
2026-05-27 09:54:54 +0100

    Bump dependencies

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ import sbtghactions.JavaSpec
 import org.typelevel.scalacoptions.ScalacOptions
 import Utils.*
 
-val scala212               = "2.12.20"
+val scala212               = "2.12.21"
 val scala213               = "2.13.18"
 val scala3                 = "3.3.7"
 val supportedScalaVersions = List(scala212, scala213, scala3)
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,19 +2,19 @@ import sbt.*
 
 object Dependencies {
   private object Versions {
-    val mongodb            = "5.6.4"
+    val mongodb            = "5.7.0"
     val fs2                = "3.13.0"
     val kindProjector      = "0.13.4"
     val circe              = "0.14.15"
-    val zio                = "2.1.24"
+    val zio                = "2.1.26"
     val zioInteropReactive = "2.0.2"
-    val zioJson            = "0.9.0"
+    val zioJson            = "0.9.2"
 
     val logback   = "1.5.32"
-    val scalaTest = "3.2.19"
+    val scalaTest = "3.2.20"
 
     val embeddedMongo   = "4.24.0"
-    val immutableValue  = "2.12.1"
+    val immutableValue  = "2.12.2"
     val commonsCompress = "1.28.0"
     val jsr305          = "3.0.2"
   }
